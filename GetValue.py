5c516b17af820b0a50c3de12e48dfe8a8fa4900e
HeapShengOoi <heapsheng_ooi@yahoo.com>
2021-07-15 21:43:10 +0800

    Added descriptions on GPIO UART alternative pins.

--- GetValue.py
+++ GetValue.py
@@ -6,17 +6,27 @@ You need to enable Serial port in RaspberryPi first:
 -  Serial Port
 -  Would you like the serial port hardware to be enabled? [YES]
 -  Accept and reboot RaspberryPi.
+
+You can check which GPIO pins that corresponds to the UART pins
+- Run RaspberryPi Terminal
+- dtoverlay -h uart1
+
+https://www.programmersought.com/article/92047061453/
+https://www.raspberrypi.org/documentation/configuration/uart.md
+https://pinout.xyz/pinout/uart
+https://github.com/UedaTakeyuki/mh-z19
+
 '''
 
 import serial
 import time
 
-ser = serial.Serial('/dev/serial0', 9600) #ttyAMA0
+ser = serial.Serial('/dev/serial0', 9600) #serial0, ttyAMA1, ttyAMA2
 
 
 while True:
     ser.write(b"\xff\x01\x86\x00\x00\x00\x00\x00\x79")
-    time.sleep(2)
+    time.sleep(5)
     response = ser.read(9)
     print(response)
     if response[0] == ord("\xff") and response[1] == ord("\x86"):
